df97dc473279e789b638563316496aabc3ef9382
wail-2002 <wail-2002@hotmail.com>
2026-01-28 11:05:52 +0100

    Update MyFirstJavaCode.java

--- src/Lesson01/MyFirstJavaCode.java
+++ src/Lesson01/MyFirstJavaCode.java
@@ -2,7 +2,7 @@ package Lesson01;
 
 public class MyFirstJavaCode {
     public static void main(String[] args) {
-        System.out.println("hello i´m Wail");
+        System.out.println("hello i´m Wail2");
     }
 
 }

7feb08db82be7ffec9209f5331a2512a69f3f289
wail-2002 <wail-2002@hotmail.com>
2026-01-29 08:45:59 +0100

    first commit

--- src/Lesson01/MyFirstJavaCode.java
+++ src/Lesson01/MyFirstJavaCode.java
@@ -2,7 +2,7 @@ package Lesson01;
 
 public class MyFirstJavaCode {
     public static void main(String[] args) {
-        System.out.println("hello i´m Wail2");
+        System.out.println("hello i´m Wail");
     }
 
 }
